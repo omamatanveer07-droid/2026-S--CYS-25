dff0ce8a16267596cf2fed160ef9e5a1ec224f0e
Omama Tanveer <omamatanveer07@gmail.com>
2026-07-01 01:05:25 +0500

    Add files via upload

---
+++ week_14/Task 1.py
@@ -0,0 +1,8 @@
+# Task 1.py
+def welcome_user():
+    print("Welcome to class!!")
+def welcome_user_again():
+    print("Welcome to class2!!")
+if __name__ == "__main__":
+    welcome_user()
+    welcome_user_again()("*\t" * row_stars))
---
+++ week_14/Task 2.py
@@ -0,0 +1,7 @@
+# Task 2.py
+def welcome_user():
+    print("Welcome to class!!")
+def welcome_user_again():
+    print("Welcome to class2!!")
+if __name__ == "__main__":
+    welcome_user()
